358ac2afac848776369e119beefc08149f18e221
zhengxiaogang <zhengxiaogang517@gmail.com>
2021-12-07 14:21:29 +0800

    12-7-14

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -7,4 +7,4 @@ class BookInfo(models.Model):
 class PeopleInfo(models.Model):
     name=models.CharField(max_length=10)
     gender=models.BooleanField()
-    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE())
+    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
