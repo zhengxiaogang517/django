561180fbf0239890b5d7939f7d2d72c93dc9fdf5
zhengxiaogang <zhengxiaogang517@gmail.com>
2021-12-07 23:33:18 +0800

    23.33

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpRequest
 def index(request):
     context={
-        'name':'有朝一日权在手，杀尽天下负我狗'
+        'name':'有朝一日权在手，杀尽天下负我'
     }
     return render(request ,'book/index.html',context=context)
